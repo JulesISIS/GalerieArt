c9ecf65d9a098e12d988a0927f9ed9150805d49f
mjule <mjule@Gemini>
2021-01-11 15:37:18 +0100

    ExpositionRepository

--- src/main/java/galerie/dao/ExpositionRepository.java
+++ src/main/java/galerie/dao/ExpositionRepository.java
@@ -7,11 +7,23 @@ package galerie.dao;
 
 import galerie.entity.Exposition;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  *
  * @author mjule
  */
 public interface ExpositionRepository extends JpaRepository<Exposition, Integer> {
-    
+
+    /**
+     * Calculer le chiffre d'affaires pour une exposition
+     *
+     * @param id la clé primaire de l'exposition
+     * @return le chiffre d'affaires de cette exposition
+     */
+    @Query("SELECT SUM(id.prixVente) AS PrixVente "
+            + "FROM Transaction t "
+            + "JOIN t.exposition e "
+            + "WHERE e.id = id ")
+    float chiffreAffairePour(Integer id);
 }
